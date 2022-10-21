7a267b30c9c07d9a6ef2cf7eb15838a012d46159
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-21 02:22:32 -0500

    Cleanup the Video Codecs & rename Spark.

--- src/main/java/xyz/e3ndr/athena/types/VideoCodec.java
+++ src/main/java/xyz/e3ndr/athena/types/VideoCodec.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum VideoCodec {
-    H264("h264", "h264_nvenc"),
-    HEVC("hevc", "hevc_nvenc"),
-    SORENSON_SPARK("flv1", null),
+    // @formatter:off
+    H264  ("h264", "h264_nvenc"),
+    HEVC  ("hevc", "hevc_nvenc"),
+    SPARK ("flv1", null),
     THEORA("libtheora", null),
+    // @formatter:on
 
     ;
 
