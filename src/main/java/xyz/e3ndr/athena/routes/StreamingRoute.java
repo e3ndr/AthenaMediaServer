132e8cdf08e5783207836c078f8db993ea393ecd
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-21 02:51:43 -0500

    Add MKV support.
    See the note in ContainerFormat.

--- src/main/java/xyz/e3ndr/athena/routes/StreamingRoute.java
+++ src/main/java/xyz/e3ndr/athena/routes/StreamingRoute.java
@@ -57,6 +57,9 @@ public class StreamingRoute implements HttpProvider {
         );
 
         String mimeType = String.format("video/%s", containerFormat.name()).toLowerCase();
+        if (containerFormat == ContainerFormat.MKV) {
+            mimeType = mimeType.replace("mkv", "x-matroska");
+        }
 
         HttpResponse resp = HttpResponse.newFixedLengthResponse(StandardHttpStatus.OK, HttpResponse.EMPTY_BODY)
             .setMimeType(mimeType);
--- src/main/java/xyz/e3ndr/athena/types/ContainerFormat.java
+++ src/main/java/xyz/e3ndr/athena/types/ContainerFormat.java
@@ -9,11 +9,20 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum ContainerFormat {
     // @formatter:off
-    MP4 ("mp4", Arrays.asList("-movflags", "faststart+frag_keyframe")),
-    FLV ("flv", Collections.emptyList()),
-    OGG ("ogg", Collections.emptyList()),
-    WEBM("webm", Collections.emptyList()),
+    MP4  ("mp4",      Arrays.asList("-movflags", "faststart+frag_keyframe")),
+    FLV  ("flv",      Collections.emptyList()),
+    OGG  ("ogg",      Collections.emptyList()),
+    WEBM ("webm",     Collections.emptyList()),
+    MKV  ("matroska", Collections.emptyList()),
     // @formatter:on
+
+    // Needs more testing but a lot of browsers accidentally support Matroska since
+    // Webm is based on it. Needs more testing by it could be a good way to give the
+    // browser more metadata. (It works because the headers for Webm & Matroska are
+    // the same, and the browser will actually do a content probe instead of just
+    // trusting the mime type. This is an easy way of getting all the benefits of
+    // Webm whilst still supporting H264 and HEVC)
+
     ;
 
     public final String ff;
