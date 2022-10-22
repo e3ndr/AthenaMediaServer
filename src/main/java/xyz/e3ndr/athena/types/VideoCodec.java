2bcc1bdb0b6e6313dbf5765befaeec8de55f05de
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-22 04:02:29 -0500

    Fix some formatting :^)

--- src/main/java/xyz/e3ndr/athena/types/VideoCodec.java
+++ src/main/java/xyz/e3ndr/athena/types/VideoCodec.java
@@ -5,11 +5,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum VideoCodec {
     // @formatter:off
-    H264  ("h264", "h264_nvenc"),
-    HEVC  ("hevc", "hevc_nvenc"),
-    SPARK ("flv1", null),
+    H264  ("h264",      "h264_nvenc"),
+    HEVC  ("hevc",      "hevc_nvenc"),
+    SPARK ("flv1",      null),
     THEORA("libtheora", null),
-    VP8   ("vp8", null),
+    VP8   ("vp8",       null),
     // @formatter:on
 
     ;
