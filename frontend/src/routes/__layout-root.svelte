8b08b2034094a43609ac4c9d24bb1e9ed13fc70a
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-30 08:04:09 -0500

    Frontend: Rethink colors to allow for future theming.

---
+++ frontend/src/routes/__layout-root.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '$lib/css/app.css';
+
+	import '$lib/css/theme/slate-tomato-dark.css';
+
+	import '$lib/css/colors/base.css';
+	import '$lib/css/colors/primary.css';
+</script>
+
+<div id="css-intermediate" class="h-full bg-base-1 text-base-12 dark-theme">
+	<slot />
+</div>
