b38c2e9fbe6369bae71e1faea52f4bf4ff423921
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-30 08:34:28 -0500

    Frontend: Expose the API as a debug variable.

--- frontend/src/routes/__layout-root.svelte
+++ frontend/src/routes/__layout-root.svelte
@@ -5,6 +5,13 @@
 
 	import '$lib/css/colors/base.css';
 	import '$lib/css/colors/primary.css';
+
+	import { onMount } from 'svelte';
+	import * as Api from '$lib/api.mjs';
+
+	onMount(() => {
+		window.debug_Api = Api;
+	});
 </script>
 
 <div id="css-intermediate" class="h-full bg-base-1 text-base-12 dark-theme">
