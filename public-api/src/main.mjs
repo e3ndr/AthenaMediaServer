ba7366c0f96a7e8210ab564f47b6e92eca537875
Ender <33337309+e3ndr@users.noreply.github.com>
2023-09-21 17:15:04 -0500

    Public API: Add an error handler.

--- public-api/src/main.mjs
+++ public-api/src/main.mjs
@@ -2,12 +2,17 @@ import * as env from "./env.mjs";
 
 export default {
     fetch: async function (request, _env) {
-        console.log("Incoming request!");
-        env.configure(_env);
+        try {
+            console.log("Incoming request!");
+            env.configure(_env);
 
-        console.log("Handling request...");
+            console.log("Handling request...");
 
-        const router = (await import("./routes/index.mjs")).default;
-        return await router.handle(request, env);
+            const router = (await import("./routes/index.mjs")).default;
+            return await router.handle(request, env);
+        } catch (e) {
+            console.error("Internal error:", e.toString(), e.stack);
+            return new Response("Internal Error.");
+        }
     },
 };
